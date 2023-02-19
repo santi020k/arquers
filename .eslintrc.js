09f6380077b819f0de39df2cba76ce55eb076a1e
Santiago Molina <santi020k@gmail.com>
2023-02-19 00:04:40 -0500

    Add pull request template change next node version

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,19 +9,15 @@ module.exports = {
     'next',
     '@react-native-community/eslint-config', // Default RN config
     'standard-with-typescript',
-    'eslint-config-prettier'
+    'eslint-config-prettier',
   ],
   parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint',
-    'react',
-    'react-native'
-  ],
+  plugins: ['@typescript-eslint', 'react', 'react-native'],
   parserOptions: {
-    'ecmaFeatures': {
-      'jsx': true
+    ecmaFeatures: {
+      jsx: true,
     },
-    'project': './tsconfig.json' // Required for Standard plugin
+    project: './tsconfig.json', // Required for Standard plugin
   },
   env: {
     'react-native/react-native': true,
@@ -31,32 +27,42 @@ module.exports = {
     // Recommended rules
     'react-native/no-unused-styles': 'warn',
     'react-native/no-inline-styles': 'error',
-    'react-native/no-raw-text': ['warn', {
-      'skip': ['TextLink', 'Link', 'A', 'title']
-    }],
+    'react-native/no-raw-text': [
+      'warn',
+      {
+        skip: ['TextLink', 'Link', 'A', 'title'],
+      },
+    ],
     'react-native/no-single-element-style-arrays': 'warn',
     'object-curly-spacing': 'error',
     '@typescript-eslint/explicit-function-return-type': 0,
     '@typescript-eslint/strict-boolean-expressions': 0,
     '@typescript-eslint/no-floating-promises': 0,
     '@typescript-eslint/no-unused-vars': 0,
-    '@typescript-eslint/require-array-sort-compare': ['error', {
-      'ignoreStringArrays': true
-    }],
-    'react/jsx-curly-spacing': ['error', {
-      'when': 'always',
-      'allowMultiline': true,
-      'children': true
-    }],
+    '@typescript-eslint/require-array-sort-compare': [
+      'error',
+      {
+        ignoreStringArrays: true,
+      },
+    ],
+    'react/jsx-curly-spacing': [
+      'error',
+      {
+        when: 'always',
+        allowMultiline: true,
+        children: true,
+      },
+    ],
     'eol-last': ['error', 'always'],
     'no-multiple-empty-lines': 'error',
-    'semi': ['error', 'never'],
+    semi: ['error', 'never'],
     // Indent with 2 spaces
-    'indent': ['error', 2],
+    indent: ['error', 2],
     // Indent JSX with 2 spaces
     'react/jsx-indent': ['error', 2],
     // Indent props with 2 spaces
     'react/jsx-indent-props': ['error', 2],
-    "react/react-in-jsx-scope": 0
-  }
+    'react/react-in-jsx-scope': 0,
+    '@typescript-eslint/no-explicit-any': 'error',
+  },
 }
